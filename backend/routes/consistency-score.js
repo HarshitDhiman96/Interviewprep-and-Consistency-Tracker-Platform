8708aa952a3b8e3b77a773d486f67810ea059eeb
Harshit Dhiman <harshitdhimanofficial78@gmail.com>
2026-04-07 20:25:10 +0530

    Revert "fixed frontend and backend folder str"
    This reverts commit e425b9b4b980bdd8435fff30618b10114fe8e00d.

--- backend/routes/consistency-score.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const consistency=require("../controllers/consistency-score");
-const authMiddleware = require("../middleware/auth-middleware");
-
-const consistencyscore=express.Router();
-
-consistencyscore.get("/consistency",authMiddleware,consistency);
-
-module.exports=consistencyscore;
--- backend/routes/progress-weekly.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const progressweekly=require("../controllers/progressweekly");
-const authMiddleware = require('../middleware/auth-middleware');
-
-const progress=express.Router();
-
-progress.get('/progress',authMiddleware,progressweekly);
-
-module.exports=progress;
